15be284813e4f97f645cf1fedc82963b2d509529
Raphael Pora <raphael.pora@gmail.com>
2023-08-17 10:11:53 +0200

    add some great bindings from theprimeagen

--- lua/rpora/remap.lua
+++ lua/rpora/remap.lua
@@ -1,1 +1,18 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+-- Files explorer
+vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+-- Move selection up/down
+vim.keymap.set("v", "K", ":m '<-2/<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- Scroll at center of the screen 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep search terms in the middle of the screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Don't put the replaced value inside the register when pasted
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
